93a98e0b879abc7ddbaf72c59a1cb1cf8d7f179d
Win Nguyen <233582892+winnguyen1905-aladin@users.noreply.github.com>
2023-06-22 14:16:16 +0700

    update eureka client & open feign

--- src/main/java/winnguyen1905/cart/core/controller/CartController.java
+++ src/main/java/winnguyen1905/cart/core/controller/CartController.java
@@ -1,7 +1,7 @@
 package winnguyen1905.cart.core.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController; 
+import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -27,30 +27,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 @RequestMapping("${release.api.prefix}/carts")
 public class CartController {
 
-    private final CartService cartService;
-    private final CartItemService cartItemService;
+  private final CartService cartService;
+  private final CartItemService cartItemService;
 
-    @PostMapping
-    @MetaMessage(message = "Add product to cart successfully")
-    public ResponseEntity<Cart> addToCart(@RequestBody @Valid AddToCartRequest addToCartRequest) {
-        UUID customerId = SecurityUtils.getCurrentUserId()
-                .orElseThrow(() -> new CustomRuntimeException("Not found user"));
-        return ResponseEntity.status(HttpStatus.CREATED.value()).body(this.cartService.handleAddToCart(customerId, addToCartRequest));
-    }
+  @PostMapping
+  @MetaMessage(message = "Add product to cart successfully")
+  public ResponseEntity<Cart> addToCart(@RequestBody @Valid AddToCartRequest addToCartRequest) {
+    UUID customerId = SecurityUtils.getCurrentUserId().orElseThrow(() -> new CustomRuntimeException("Not found user"));
+    return ResponseEntity.status(HttpStatus.CREATED.value())
+        .body(this.cartService.handleAddToCart(customerId, addToCartRequest));
+  }
 
-    @PatchMapping
-    @MetaMessage(message = "Update cart successfully")
-    public ResponseEntity<CartItem> updateCartItem(@RequestBody CartItem cartItemDTO) {
-        UUID customerId = SecurityUtils.getCurrentUserId()
-                .orElseThrow(() -> new CustomRuntimeException("Not found user"));
-        return ResponseEntity.ok().body(this.cartItemService.handleUpdateCartItem(cartItemDTO, customerId));
-    }
+  @PatchMapping
+  @MetaMessage(message = "Update cart successfully")
+  public ResponseEntity<CartItem> updateCartItem(@RequestBody CartItem cartItemDTO) {
+    UUID customerId = SecurityUtils.getCurrentUserId().orElseThrow(() -> new CustomRuntimeException("Not found user"));
+    return ResponseEntity.ok().body(this.cartItemService.handleUpdateCartItem(cartItemDTO, customerId));
+  }
 
-    @GetMapping
-    @MetaMessage(message = "Get my card successfully")
-    public ResponseEntity<Cart> getMethodName(Pageable pageable) {
-        UUID customerId = SecurityUtils.getCurrentUserId().orElseThrow(() -> new CustomRuntimeException("Not found user"));
-        return ResponseEntity.ok().body(this.cartService.handleGetMyCartDetails(customerId, pageable));
-    }
+  @GetMapping
+  @MetaMessage(message = "Get my card successfully")
+  public ResponseEntity<Cart> getMethodName(Pageable pageable) {
+    UUID customerId = SecurityUtils.getCurrentUserId().orElseThrow(() -> new CustomRuntimeException("Not found user"));
+    return ResponseEntity.ok().body(this.cartService.handleGetMyCartDetails(customerId, pageable));
+  }
 
 }
