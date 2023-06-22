7c98cc6b95cebd2376d109e7f6bc954615d8307b
Win Nguyen <233582892+winnguyen1905-aladin@users.noreply.github.com>
2023-06-22 21:51:49 +0700

    mapper builder

---
+++ src/main/java/winnguyen1905/cart/core/mapper/CartMapper.java
@@ -0,0 +1,63 @@
+package winnguyen1905.cart.core.mapper;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import winnguyen1905.cart.core.model.request.ProductVariantByShopContainer;
+import winnguyen1905.cart.core.model.response.CartResponse;
+import winnguyen1905.cart.core.model.response.PriceStatisticsResponse;
+import winnguyen1905.cart.persistance.entity.ECartItem;
+
+public class CartMapper {
+  private record CartItemsWithTotal(List<CartResponse.CartItem> cartItems, double totalPrice) {}
+
+  public static final CartResponse with(HashMap<UUID, ECartItem> mapECartItem, ProductVariantByShopContainer cartByShopProductResponse) {
+    List<CartResponse.CartByShop> cartByShops = cartByShopProductResponse.shopProductVariants().stream()
+        .map(shopProductVariant -> mapToCartByShop(mapECartItem, shopProductVariant))
+        .collect(Collectors.toList());
+        
+    return new CartResponse(cartByShops);
+  }
+
+  private static CartResponse.CartByShop mapToCartByShop(
+      HashMap<UUID, ECartItem> mapECartItem, 
+      ProductVariantByShopContainer.ShopProductVariant shopProductVariant) {
+    
+    var cartItemsWithTotal = calculateCartItemsAndTotal(mapECartItem, shopProductVariant);
+    
+    return CartResponse.CartByShop.builder()
+        .priceStatistic(PriceStatisticsResponse.builder()
+            .totalPrice(cartItemsWithTotal.totalPrice())
+            .finalPrice(cartItemsWithTotal.totalPrice())
+            .build())
+        .cartItems(cartItemsWithTotal.cartItems())
+        .shopId(shopProductVariant.shopId())
+        .build();
+  }
+
+  private static CartItemsWithTotal calculateCartItemsAndTotal(
+      HashMap<UUID, ECartItem> mapECartItem,
+      ProductVariantByShopContainer.ShopProductVariant shopProductVariant) {
+    
+    double totalPrice = 0;
+    List<CartResponse.CartItem> cartItems = new ArrayList<>();
+
+    for (var productVariantReview : shopProductVariant.productVariantReviews()) {
+      ECartItem cartItem = mapECartItem.get(productVariantReview.id());
+      double itemPrice = productVariantReview.price() * cartItem.getQuantity();
+      totalPrice += itemPrice;
+
+      cartItems.add(CartResponse.CartItem.builder()
+          .price(itemPrice)
+          .quantity(cartItem.getQuantity())
+          .isSelected(!cartItem.getIsDeleted())
+          .productVariantReview(productVariantReview)
+          .build());
+    }
+
+    return new CartItemsWithTotal(cartItems, totalPrice);
+  }
+}
