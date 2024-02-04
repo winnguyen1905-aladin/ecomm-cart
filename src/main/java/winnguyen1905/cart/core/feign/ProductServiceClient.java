9a21de8032b1ef0cc14351b6564b73bdec86ea27
Win Nguyen <233582892+winnguyen1905-aladin@users.noreply.github.com>
2024-02-04 17:43:06 +0700

    service to service comunicate

---
+++ src/main/java/winnguyen1905/cart/core/feign/ProductServiceClient.java
@@ -0,0 +1,26 @@
+package winnguyen1905.cart.core.feign;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+import winnguyen1905.cart.core.model.request.ProductVariantByShopContainer;
+
+@FeignClient(name = "PRODUCT-SERVICE", url = "http://localhost:8090")
+public interface ProductServiceClient {
+
+  @GetMapping("products/variant-detail/{ids}")
+  ProductVariantByShopContainer getProductCartDetail(@PathVariable Set<UUID> ids);
+
+  // @PostMapping("inventory/update-redis")
+  // ResponseEntity<?> updateInventory(
+  //         @RequestBody Object inventory, @RequestHeader("service") String service);
+
+  // @GetMapping("inventory/check")
+  // ResponseEntity<ProductInventory> checkInventory(
+  //         @RequestParam("id") int variantId, @RequestHeader("service") String service);
+}
