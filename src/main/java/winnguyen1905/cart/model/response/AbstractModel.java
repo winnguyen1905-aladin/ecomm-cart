d35c23def9765c363e839b32fdf0c16339b4f08f
Win Nguyen <233582892+winnguyen1905-aladin@users.noreply.github.com>
2024-12-07 13:19:41 +0700

    Remove PromotionApplyRequest and PromotionApplyWrapperRequest classes; introduce new AbstractModel interface and CartItemResponse, CartResponse models for enhanced cart response handling.

---
+++ src/main/java/winnguyen1905/cart/model/response/AbstractModel.java
@@ -0,0 +1,16 @@
+package winnguyen1905.cart.model.response;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+// @JsonInclude(value = Include.NON_NULL)
+@JsonNaming(PropertyNamingStrategies.LowerCamelCaseStrategy.class)
+@JsonFormat(pattern = "HH-mm-ss a dd-MM-yyyy", timezone = "GMT+7")
+public interface AbstractModel extends Serializable {
+  @Serial
+  static final long serialVersionUID = 7213600440729202783L;
+}
